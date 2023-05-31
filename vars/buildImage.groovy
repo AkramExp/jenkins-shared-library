a1c522f85b34f026f3b2a22faed85858cf0b4844
Akram Ali <92240584+AkramExp@users.noreply.github.com>
2023-05-31 12:23:51 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "building the image"
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
         sh 'docker build -t akramexp/my-repo:jma-3.0'
         sh 'docker login -u $USER -p $PASSWORD'
         sh 'docker push akramexp/my-repo:jma-3.0'
